bb58e0055abb2620ccf208e869028b139a3f8210
lenovo <minghuiwu2023@gmail.com>
2026-02-24 14:36:17 -0800

    Remove enableSystem to fix initial light mode flash

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem
             disableTransitionOnChange
           >
             {/* Background image layer */}
